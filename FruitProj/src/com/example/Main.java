21a824fc96462cbfacd20529af5697512dcdb1aa
achel03 <khl3543@naver.com>
2021-09-11 08:24:46 +0900

    add 10 Fruits

--- FruitProj/src/com/example/Main.java
+++ FruitProj/src/com/example/Main.java
@@ -12,7 +12,14 @@ public class Main {
 		fl.add(s1);
 		Fruit s2 = new Fruit(102,"Orange",1600);
 		fl.add(s2);
-		fl.add(new Fruit(103, "Kiwi",2000));
+		fl.add(new Fruit(103, "Grape",3000));
+		fl.add(new Fruit(104, "Cherry",500));
+		fl.add(new Fruit(105, "Date Palm",1300));
+		fl.add(new Fruit(106, "Kiwi",4000));
+		fl.add(new Fruit(107, "Persimon",5000));
+		fl.add(new Fruit(108, "Melon",15000));
+		fl.add(new Fruit(109, "Banana",2500));
+		fl.add(new Fruit(110, "Strawberry",7700));
 		
 		Collections.sort(fl, new FruitComparator());
 		for(Fruit s : fl) {
